7f7fa96d08671cd4f9e7c4959944da865b73ba6e
Nitish Deshmukh <nitishdeshmukh24@gmail.com>
2025-12-16 19:12:42 +0530

    chore(tailwind): migrate from v3 to v4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold underline text-center pt-10">
-        Hello MERN + Tailwind!
-      </h1>
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-      </Routes>
-    </div>
+    <h1 className="text-3xl font-bold text-red-600">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
