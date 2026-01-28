d1d9f7e2c268c1d54a4d92ebc8a954a7074ec6d2
Nitish Deshmukh <nitishdeshmukh24@gmail.com>
2026-01-29 00:33:15 +0530

    code

--- client/src/pages/auth/sign-in/components/user-auth-form.tsx
+++ client/src/pages/auth/sign-in/components/user-auth-form.tsx
@@ -5,7 +5,7 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Loader2, LogIn } from 'lucide-react'
 import { Link, useNavigate } from 'react-router'
 import { toast } from 'sonner'
-import { IconFacebook, IconGithub } from '@/assets/brand-icons'
+import { IconGoogle } from '@/assets/brand-icons'
 import { useAuthStore } from '@/stores/auth-store'
 import { sleep, cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
@@ -150,22 +150,14 @@ export function UserAuthForm({
                     </div>
                 </div>
 
-                <div className='grid grid-cols-2 gap-2'>
-                    <Button
-                        variant='outline'
-                        type='button'
-                        disabled={isLoading}
-                    >
-                        <IconGithub className='h-4 w-4' /> GitHub
-                    </Button>
-                    <Button
-                        variant='outline'
-                        type='button'
-                        disabled={isLoading}
-                    >
-                        <IconFacebook className='h-4 w-4' /> Facebook
-                    </Button>
-                </div>
+                <Button
+                    variant='outline'
+                    type='button'
+                    disabled={isLoading}
+                    className='w-full'
+                >
+                    <IconGoogle className='h-4 w-4' /> Google
+                </Button>
             </form>
         </Form>
     )
